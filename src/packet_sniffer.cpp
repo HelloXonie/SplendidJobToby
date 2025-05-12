b1e4b675afcfc9497eb6df7dae7c8f4b617ca8c8
HelloXonie <126190219+HelloXonie@users.noreply.github.com>
2025-05-11 18:49:06 -0700

    added some functionality to the packet sniffer header and cpp files

---
+++ src/packet_sniffer.cpp
@@ -0,0 +1,10 @@
+#include "packet_sniffer.h"
+#include <iostream>
+
+void PacketSniffer::startCapture() {
+	std::cout << "Starting packet capture..." << std::endl;
+}
+
+void PacketSniffer::processPacket() {
+	std::cout << "Processing packet..." << std::endl;
+}
